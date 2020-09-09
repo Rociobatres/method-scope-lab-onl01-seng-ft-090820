8301163236b53b2ef829dbd3b2c5768de39cf144
Rocio Batres <jonasgirl21@gmail.com>
2020-09-09 21:40:01 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,6 @@
 def 
   "rescued"
-  "Hooray! Mario has rescued Princess Peach."
+  puts "Hooray! Mario has rescued Princess Peach."
 end
 
 ## Comment the below back in to see method scope in action!
